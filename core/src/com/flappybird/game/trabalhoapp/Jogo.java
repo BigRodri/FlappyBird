16effabbbd4c1c035fb6738c9514fd706060fadb
BigRodri <rodrigonnes@icloud.com>
2021-05-10 14:38:22 -0300

    Atualização 10/05/2021 14:38

--- core/src/com/flappybird/game/trabalhoapp/Jogo.java
+++ core/src/com/flappybird/game/trabalhoapp/Jogo.java
@@ -1,31 +1,55 @@
 package com.flappybird.game.trabalhoapp;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Jogo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    //Construção dos assets
+	private SpriteBatch batch;
+	private Texture passaro;
+	private Texture background;
+
+	//Movimentação
+	private int movimentaY = 0;
+	private int movimentaX = 0;
+
+	//Atribuindo variaveis para o redimensionar o background
+	private float larguraDispositivo;
+	private float alturaDispositivo;
+
+
 	@Override
 	public void create () {
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		passaro = new Texture("passaro1.png");
+		background = new Texture("fundo.png");
+
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		batch.draw(background, 0, 0, larguraDispositivo, alturaDispositivo);
+		batch.draw(passaro,50,50, movimentaX, movimentaY);
+
+		movimentaX++;
+		movimentaY++;
+
 		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
